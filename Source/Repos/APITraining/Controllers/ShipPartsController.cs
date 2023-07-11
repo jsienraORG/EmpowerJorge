4aef088ad1e1412fbae8b97b62bb743facb57d1c
jsienra <133407643+jsienra@users.noreply.github.com>
2023-07-11 09:41:51 -0400

    Add project files.

---
+++ Source/Repos/APITraining/Controllers/ShipPartsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace APItraining.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipPartsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetParts()
+        {
+            string[] parts = { "Flux capacitor", "Inertial damper", "Impulse nacelle" };
+
+            if (parts.Any())
+                return NotFound();
+            return Ok(parts);
+        }
+
+        [HttpPost]
+        public ActionResult AddParts()
+        {
+
+        }
+
+        [HttpUpdate]
+        public ActionResult UpdateParts()
+        {
+
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteParts()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Source/Repos/EmpowerJorge/README.md
@@ -0,0 +1,1 @@
+# EmpowerScript1
